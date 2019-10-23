23ab394533340e3f9c400946bfea7344db0864a3
root <root@vm-lkq-AEM-pipeline-test.hzrgl32xqnneznqaddsyhmmuqd.shax.internal.chinacloudapp.cn>
2019-10-23 09:24:16 +0800

    background-color red

--- bgd-app/index.js
+++ bgd-app/index.js
@@ -30,7 +30,7 @@ app.all('*', function(req, res) {   // serve all other requests
   var app_obj = JSON.parse(vcap_app)
   var icon_name = (app_obj.application_name.indexOf("blue")>= 0)?"Blue-station.png":"Green-station.png";
   res.writeHead(200, {"Content-Type": "text/html; charset=UTF-8"});
-  res.write("<html><body style='font-family: Arial'><img align='left' src='./images/Blue-Green-icon.png'>");
+  res.write("<html><body style='font-family: Arial; background-color: red'><img align='left' src='./images/Blue-Green-icon.png'>");
   res.write("<h1><br><br><br>&nbsp;&nbsp;Blue-Green deployments 2019-05-07 v1.2</h1><hr>");
   res.write("<p><img src='./images/"+icon_name+"'></p>");
   res.write("<hr>");
